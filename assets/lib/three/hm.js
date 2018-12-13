1126558f937c03c38c3b2720fde3deb2eda4a256
songjiang <songjiang@100tal.com>
2018-12-13 10:23:04 +0800

    delete

--- assets/lib/three/hm.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?b2c1bbeb1bb5f7f95f6d7c53be416580";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
